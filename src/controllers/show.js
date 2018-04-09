f20579287d82e522b7ff26a155ac3f12a7d73503
Oussama Barkouki <oussama1598@gmail.com>
2018-04-09 12:43:02 -0700

    Added Utorrent basic api

--- src/controllers/show.js
+++ src/controllers/show.js
@@ -1,57 +1,56 @@
-import _ from 'underscore'
-import Transmission from 'modules/Transmission'
-import Show from 'modules/Show'
-import Prompt from 'modules/Prompt'
-import error from 'console-error'
-import warn from 'console-warn'
+import _ from 'underscore';
+import Transmission from 'modules/Transmission';
+import Utorrent from 'modules/Utorrent';
+import Show from 'modules/Show';
+import Prompt from 'modules/Prompt';
+import error from 'console-error';
+import warn from 'console-warn';
 
-const DIR = process.cwd()
-const transmission = new Transmission()
-const show = new Show()
-const prompt = new Prompt()
+const DIR = process.cwd();
+const transmission = new Transmission();
+const utorrent = new Utorrent('http://127.0.0.1:8888', 'admin', 'admin');
+const show = new Show();
+const prompt = new Prompt();
 
-async function filesPrompt (episodes, session) {
+async function filesPrompt(episodes, session) {
   for (const ep of episodes) {
     const torrents = _.chain(ep.torrents)
       .sortBy(ep => -ep.seeds)
-      .value()
+      .value();
 
     if (torrents.length === 0) {
-      warn(`Skipped episode ${ep.episode}, no torrents found`)
-      continue
+      warn(`Skipped episode ${ep.episode}, no torrents found`);
+      continue;
     }
 
-    const torrentMagnet = await prompt.askForTorrent(torrents)
-    transmission.addTorrent(torrentMagnet, DIR)
+    const torrentMagnet = await prompt.askForTorrent(torrents);
+    transmission.addTorrent(torrentMagnet, DIR);
   }
 }
 
-export async function searchForEpisode (name, from, to = 'f') {
-  from = isNaN(from) && from !== 'latest' ? 1 : from
-  to = isNaN(to) || to === 'f' || from === 'latest'
-    ? Infinity
-    : to
+export async function searchForEpisode(name, from, to = 'f') {
+  from = isNaN(from) && from !== 'latest' ? 1 : from;
+  to = isNaN(to) || to === 'f' || from === 'latest' ? Infinity : to;
 
-  if (from === 'latest') warn('the \'-to\' argument will be ignored, since \'latest\' is used')
+  if (from === 'latest')
+    warn("the '-to' argument will be ignored, since 'latest' is used");
 
-  try {
-    await transmission.load()
+  await utorrent.getToken();
+  // try {
+  //   await transmission.load();
 
-    const showName = name.replace(/ /g, '-')
-    await show.loadData(showName)
+  //   const showName = name.replace(/ /g, '-');
+  //   await show.loadData(showName);
 
-    const seasonNumber = from === 'latest'
-      ? show.getLastSeasonNumber()
-      : await prompt.askForSeason(show.getSeasons())
+  //   const seasonNumber =
+  //     from === 'latest'
+  //       ? show.getLastSeasonNumber()
+  //       : await prompt.askForSeason(show.getSeasons());
 
-    from = from === 'latest'
-      ? show.getLastEpisodeNumber()
-      : parseInt(from)
+  //   from = from === 'latest' ? show.getLastEpisodeNumber() : parseInt(from);
 
-    await filesPrompt(
-      show.getRangeEpisodes(seasonNumber, from, to)
-    )
-  } catch (err) {
-    return error(err.message)
-  }
+  //   await filesPrompt(show.getRangeEpisodes(seasonNumber, from, to));
+  // } catch (err) {
+  //   return error(err.message);
+  // }
 }
---
+++ src/modules/Utorrent.js
@@ -0,0 +1,27 @@
+import request from 'request-promise';
+import cheerio from 'cheerio';
+
+export default class Utorrent {
+  constructor(baseUrl, username, password) {
+    this.baseUrl = baseUrl;
+    this.username = this.username;
+    this.password = this.password;
+    this.token = null;
+  }
+
+  async getToken() {
+    const $ = await request({
+      method: 'GET',
+      uri: `${this.baseUrl}/token.html`,
+      auth: {
+        user: this.username,
+        pass: this.password
+      },
+      transform: res => cheerio.load(res)
+    });
+
+    this.token = $('#token').text();
+
+    console.log(this.token);
+  }
+}
