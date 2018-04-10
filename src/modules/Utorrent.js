bb037696629d2480c7fdc54ed2ced80da9265ad2
Oussama Barkouki <oussama1598@gmail.com>
2018-04-10 09:15:42 -0700

    Fixed adding magnet to utorrent

--- src/modules/Utorrent.js
+++ src/modules/Utorrent.js
@@ -1,7 +1,5 @@
-import request from 'request-promise';
+import request, { jar } from 'request-promise';
 import cheerio from 'cheerio';
-import { parse as parseCookie } from 'cookie';
-import { Cookie } from 'tough-cookie';
 import { URL } from 'url';
 
 export default class Utorrent {
@@ -10,45 +8,35 @@ export default class Utorrent {
     this.username = username;
     this.password = password;
     this.token = null;
+    this.jar = jar();
   }
 
   async getToken() {
-    const res = await request({
+    const $ = await request({
       method: 'GET',
       uri: `${this.baseUrl}/token.html`,
       auth: {
         user: this.username,
         pass: this.password
       },
-      resolveWithFullResponse: true
+      jar: this.jar,
+      transform: body => cheerio.load(body)
     });
-    const $ = cheerio.load(res.body);
 
-    this.guid = parseCookie(res.headers['set-cookie'][0]).GUID;
     this.token = $('#token').text();
   }
 
   addTorrent(magnet) {
-    console.log(this);
-
-    const cookie = new Cookie({
-      key: 'GUID',
-      value: this.guid,
-      domain: '127.0.0.1'
-    });
-    const jar = request.jar();
-    const uri = new URL(this.baseUrl);
+    const uri = new URL(`${this.baseUrl}/`);
 
     uri.searchParams.append('token', this.token);
     uri.searchParams.append('action', 'add-url');
     uri.searchParams.append('s', magnet);
 
-    jar.setCookie(cookie, this.baseUrl);
-
     request({
       method: 'GET',
       uri: uri.toString(),
-      jar,
+      jar: this.jar,
       auth: {
         user: this.username,
         pass: this.password
